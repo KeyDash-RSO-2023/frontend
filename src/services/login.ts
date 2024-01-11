5c03a2d697a4a281a270985f02c18a6aaa465479
Kurtaga <ak84795@student.uni-lj.si>
2024-01-11 05:28:24 +0100

    Changed USERS_URL 2

--- src/services/login.ts
+++ src/services/login.ts
@@ -2,7 +2,7 @@
 // process.env.REACT_APP_BACKEND_URL || "http://localhost:8080/get";
 
 // const USERS_URL = import.meta.env.VITE_USERS_API_URL;
-const USERS_URL = "http://20.240.34.248/users/v1/";
+const USERS_URL = "http://20.240.34.248/users/v1/users/";
 
 export const login = async (requestData) => {
   console.log("read USERS_URL", USERS_URL);
